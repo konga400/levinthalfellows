1f16bca880c5b3b4870d4dcb237725867692af6e
spenc <spencerdean.b@gmail.com>
2022-01-05 18:36:43 -0700

    firebase and tailwind init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="App">
-
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-bold text-center">Coming Soon!</h1>
     </div>
   );
 }
